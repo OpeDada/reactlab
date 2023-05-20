8adedbbd69735cd30491870ac29dea7b11a2469c
OpeDada <opedada@gmail.com>
2023-05-21 00:25:21 +0900

    separate input functionalities

--- src/App.js
+++ src/App.js
@@ -3,43 +3,53 @@ import { useState, useRef } from "react";
 import "./App.css";
 
 function App() {
-  const inputRef = useRef(null);
+  const [input1, setInput1] = useState("");
+  const [input2, setInput2] = useState("");
   const resultRef = useRef(null);
   const [result, setResult] = useState(0);
 
+  function handleInput1Change(e) {
+    setInput1(e.target.value);
+  }
+
+  function handleInput2Change(e) {
+    setInput2(e.target.value);
+  }
+
   function plus(e) {
     e.preventDefault();
-    setResult((result) => result + Number(inputRef.current.value));
+    setResult(Number(input1) + Number(input2));
   }
 
   function minus(e) {
     // Add the code for the minus function
     e.preventDefault();
-    setResult((result) => result - Number(inputRef.current.value));
+    setResult(Number(input1) - Number(input2));
   }
 
   function times(e) {
     // Add the code for the plus function
     e.preventDefault();
-    setResult((result) => result * Number(inputRef.current.value));
+    setResult(Number(input1) * Number(input2));
   }
 
   function divide(e) {
     // Add the code for the divide function
     e.preventDefault();
-    setResult((result) => result / Number(inputRef.current.value));
+    setResult(Number(input1) / Number(input2));
   }
 
   function resetInput(e) {
     // Add the code for the resetInput function
     e.preventDefault();
-    inputRef.current.value = "";
+    setInput1("");
+    setInput2("");
   }
 
   function resetResult(e) {
     // Add the code for the resetResult function
     e.preventDefault();
-    resultRef.current.innerHTML = "";
+    setResult(0);
   }
 
   return (
@@ -48,18 +58,27 @@ function App() {
         <h1>Simplest Working Calculator</h1>
       </div>
       <form>
-        <p ref={resultRef}>{inputRef.current.value}</p>
         <input
           pattern="[0-9]"
-          ref={inputRef}
+          value={input1}
           type="number"
           placeholder="Type a number"
+          onChange={handleInput1Change}
         />
+        <input
+          type="number"
+          placeholder="Type another number"
+          value={input2}
+          onChange={handleInput2Change}
+        />
+        <h2>Result: {result}</h2>
         <button onClick={plus}>Add</button>
         <button onClick={minus}>Subtract</button>
         <button onClick={times}>Multiply</button>
         <button onClick={divide}>Divide</button>
-        <button onClick={resetInput}>Reset Input</button>
+        <button onClick={resetInput} value="clear">
+          Reset Input
+        </button>
         <button onClick={resetResult}>Reset Result</button>
       </form>
     </div>
